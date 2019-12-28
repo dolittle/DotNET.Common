a7f9734cff1c4dd4073b0079893b989250a65b76
Einar Ingebrigtsen <einar@dolittle.com>
2019-12-28 15:57:26 +0100

    Clean up

--- Source/CodeAnalysis/ExceptionShouldBeSpecific/Analyzer.cs
+++ Source/CodeAnalysis/ExceptionShouldBeSpecific/Analyzer.cs
@@ -3,10 +3,7 @@
 
 using System;
 using System.Collections.Immutable;
-using System.Linq;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.Operations;
 
